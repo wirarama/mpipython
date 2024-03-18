912201581acf90abab1ce222f62fe3ac5596d773
wirarama wedashwara <wirarama@gmail.com>
2024-03-18 16:40:00 +0800

    update
    update

--- sendrecv-adv2.py
+++ sendrecv-adv2.py
@@ -8,8 +8,8 @@ print("rank %d " % rank,end=" ")
 if rank==0:
     data1 = 2
     data2 = 3
-    print("mengirim ke ",end=" ")
-    for a in range(size):
+    print("sends ",end=" ")
+    for a in range(1,size):
         if a%2==0:
             comm.send(data1,dest=a,tag=1)
             print(a,end=",")
@@ -19,21 +19,19 @@ if rank==0:
     print()
 elif (rank%2)==0:
     data3 = comm.recv(source=0,tag=1)
-    print("mengirim ke ",end=" ")
     for a in range(1,size):
         if a%2!=0:
             comm.send(data3*rank,dest=a,tag=1)
-            print(a,end=",")
+            print(">",a,data3*rank,end=",")
             data5 = comm.recv(source=a,tag=1)
-            print(">",a,data5*rank,end=" ")
+            print("<",a,data5*rank,end=",")
     print()
 else:
     data4 = comm.recv(source=0,tag=1)
-    print("mengirim ke ",end=" ")
     for a in range(1,size):
         if a%2==0:
             comm.send(data4*rank,dest=a,tag=1)
-            print(a,end=",")
+            print(">",a,data4*rank,end=",")
             data6 = comm.recv(source=a,tag=1)
-            print(">",a,data6*rank,end=" ")
+            print("<",a,data6*rank,end=",")
     print()
