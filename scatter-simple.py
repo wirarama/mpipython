a33d28799045187822a04031883e9db3c01379f8
wirarama wedashwara <wirarama@gmail.com>
2024-04-02 16:16:13 +0800

    update

--- scatter-simple.py
+++ scatter-simple.py
@@ -4,7 +4,7 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 if rank == 0:
-    data = [4,5,6,7] #its a array
+    data = [3,[4,3,6],1.6379,"beda sendiri"] #ini array
 else:
     data = None
 data = comm.scatter(data, root=0) #its not a bcast but scatter
